ae88bee8e8d5d77dd0a34288922b2ac55ca1c33e
DESKTOP-CK234V8\adamt <adamtoda01@gmail.com>
2023-07-25 19:14:52 +0200

    Project conecpt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MenuComponent} from "./navigation/nav-menu/menu.component";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {AboutComponent} from "./section/about-section/about.component";
+import {HomeComponent} from "./section/home-section/home.component";
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'home', component: HomeComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
